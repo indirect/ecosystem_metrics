c78ffee4779e043991de1718f2566d7796964ab6
Andre Arko <andre@arko.net>
2017-06-04 22:30:16 -0700

    first post

---
+++ parse_user_agents.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require 'json'
+
+input = ARGF.read.lines
+input.shift
+input.sort!
+input.uniq!
+
+agents = {}
+
+input.each do |line|
+  match = line.match(/^"bundler\/(?<bundler>[a-z0-9.]+) rubygems\/(?<rubygems>[a-z0-9.]+) ruby\/(?<ruby>[a-z0-9.]+) \((?<cpu>[^-]+)-(?<vendor>[^-]+)-(?<os>.+)\) command\/(?<command>.*?) (jruby\/(?<jruby>[a-z0-9.]+) )?(options\/(?<options>.+?) )?(ci\/(?<ci>.+) )?[a-f0-9]+( (?<extra>.+))?"$/)
+
+  if match.nil?
+    puts "No match! Line was:\n#{line}"
+    next
+  end
+
+  info = match.named_captures
+  options = info.delete("options")
+
+  options.split(",").each do |name|
+    agents["options"] ||= {}
+    agents["options"][name] ||= 0
+    agents["options"][name] += 1
+  end if options
+
+  info.each do |name, value|
+    next if value.nil?
+    agents[name] ||= {}
+    agents[name][value] ||= 0
+    agents[name][value] += 1
+  end
+end
+
+puts JSON.pretty_generate(agents)
